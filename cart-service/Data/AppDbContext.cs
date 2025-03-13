a0ae3f03d75cb9afadc762da87da28214aaa60bb
Максим Фоминцев <max135603@gmail.com>
2025-03-13 21:43:20 +0300

    Подключение к БД через EF Core

---
+++ cart-service/Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using cart_service.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace cart_service.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+    public DbSet<Product> Products { get; set; }
+    public DbSet<CartItem> CartItems { get; set; }
+}
