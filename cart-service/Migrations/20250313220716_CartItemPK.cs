442cb927aa6813cd4a0b35dffdd569b596b542ab
Максим Фоминцев <max135603@gmail.com>
2025-03-14 01:11:15 +0300

    Миграции для первичного ключа корзины

---
+++ cart-service/Migrations/20250313220716_CartItemPK.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace cart_service.Migrations
+{
+    /// <inheritdoc />
+    public partial class CartItemPK : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_CartItems",
+                table: "CartItems");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "CartItems",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer")
+                .OldAnnotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_CartItems",
+                table: "CartItems",
+                columns: new[] { "UserEmail", "ProductId" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_CartItems",
+                table: "CartItems");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "CartItems",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer")
+                .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_CartItems",
+                table: "CartItems",
+                column: "Id");
+        }
+    }
+}
