a9c879fdbc8d3571796f2880eae112abd0473d5d
Максим Фоминцев <max135603@gmail.com>
2025-03-13 23:30:39 +0300

    Возвращены репозитории

---
+++ cart-service/Repositories/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using cart_service.Models;
+
+namespace cart_service.Repositories;
+
+public interface IProductRepository
+{
+    Task<Product> GetByIdAsync(int id);
+    Task<List<Product>> GetAllAsync();
+    Task AddAsync(Product product);
+    Task UpdateAsync(Product product);
+    Task DeleteAsync(int id);
+    Task SaveChangesAsync();
+}
