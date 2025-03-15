711998c69ddd7660a622dd0fce851e4bf590cdc5
Максим Фоминцев <max135603@gmail.com>
2025-03-15 20:03:14 +0300

    Исходные данные пользователей

--- auth-service/Data/AuthDbContext.cs
+++ auth-service/Data/AuthDbContext.cs
@@ -19,4 +19,21 @@ public class AuthDbContext : DbContext
             .HasIndex(u => u.Email)
             .IsUnique();
     }
+    
+    public async Task Initialize()
+    {
+        // Проверяем, есть ли уже категории в базе данных
+        if (!Users.Any())
+        {
+            // Добавляем начальные категории
+            var users = new List<User>
+            {
+                new() { Username = "admin", Password = "admin", Email = "admin@mail.ru", Role = "Admin" },
+                new() { Username = "user", Password = "user", Email = "user@mail.ru", Role = "User" }
+            };
+
+            await Users.AddRangeAsync(users);
+            await SaveChangesAsync();
+        }
+    }
 }
