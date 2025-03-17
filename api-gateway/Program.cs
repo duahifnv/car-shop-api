1d5d209906e88134fd727a2b4cd804828d051609
Максим Фоминцев <max135603@gmail.com>
2025-03-17 19:44:18 +0300

    Настройка Swagger UI для API шлюза

--- api-gateway/Program.cs
+++ api-gateway/Program.cs
@@ -1,6 +1,34 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+// Добавляем контроллеры
+builder.Services.AddControllers();
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("auth", new OpenApiInfo { Title = "Auth Service", Version = "v1" });
+    c.SwaggerDoc("cart", new OpenApiInfo { Title = "Cart Service", Version = "v1" });
+});
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Включение Swagger UI
+app.UseSwagger();
+app.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint("/auth/swagger/v1/swagger.json", "Auth Service");
+    options.SwaggerEndpoint("/api/cart/swagger/v1/swagger.json", "Cart Service");
+    options.RoutePrefix = string.Empty;
+});
 
-app.Run();
+// Используем YARP для перенаправления запросов
+app.MapReverseProxy();
+
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run("http://*:9000");
