ccf2c2eb211e8a78f1f15f0b373a235a7bf400c6
Максим Фоминцев <max135603@gmail.com>
2025-03-18 18:03:33 +0300

    UI для пользователя (не администратора)

---
+++ desktop-client/ViewModels/UserViewModel.cs
@@ -0,0 +1,25 @@
+﻿using desktop_client.Services;
+
+namespace desktop_client.ViewModels;
+
+public class UserViewModel
+{
+    private readonly ApiService _apiService;
+    private readonly string _userEmail;
+
+    public UserViewModel(ApiService apiService, string userEmail)
+    {
+        _apiService = apiService;
+        _userEmail = userEmail;
+    }
+
+    public async Task<CartResponse> GetMyCartAsync()
+    {
+        return await _apiService.GetCartAsync(_userEmail);
+    }
+
+    public async Task<List<ProductResponse>> GetProductsAsync()
+    {
+        return await _apiService.GetProductsAsync();
+    }
+}
---
+++ desktop-client/Views/UserView.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using desktop_client.Services;
+using desktop_client.ViewModels;
+
+namespace desktop_client.Views;
+
+public partial class UserView : UserControl
+{
+    private readonly UserViewModel _viewModel;
+
+    public UserView(string token, string userEmail)
+    {
+        InitializeComponent();
+        _viewModel = new UserViewModel(new ApiService(), userEmail);
+        DataContext = _viewModel;
+        LoadData();
+    }
+
+    private async void LoadData()
+    {
+        var cart = await _viewModel.GetMyCartAsync();
+        var products = await _viewModel.GetProductsAsync();
+
+        // Привязка данных к DataGrid
+        CartDataGrid.ItemsSource = cart.Items;
+        ProductsDataGrid.ItemsSource = products;
+    }
+}
